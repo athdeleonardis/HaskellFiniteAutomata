da342a19eac4936b148efafb2b44663305950d8f
athdeleonardis <athdeleonardis@gmail.com>
2024-12-05 11:49:39 +1100

    Turing machine can now step through strings. TMTape: Exported functions. TM: Turing machines can now be stepped using the new functions, including tmInitialState, tmStep, tmSimulate and tmCheckString.

--- src/Automata/TuringMachine/TM.hs
+++ src/Automata/TuringMachine/TM.hs
@@ -12,11 +12,19 @@ module FiniteAutomata.Automata.TuringMachine.TM
 )
 where
 
-data TMMove = Left | Right
+import FiniteAutomata.Automata.TuringMachine.TMTape
+
+data TMMove = TMLeft | TMRight
+
+instance Show TMMove where
+  show TMLeft = "Left"
+  show TMRight = "Right"
 
 type TMRuleKey a b = (a, b)
 type TMRuleVal a b = (a, b, TMMove)
 type TMRule a b = (TMRuleKey a b, TMRuleVal a b)
+
+type TMState a b = (a, TMTape b)
 
 -- |Turing Machine. (qs, ts, b, is, rs, q0, fs) represents a TM with states 'qs', tape symbols 'ts', tape blank symbol 'b', tape input symbols 'as', rules 'rs', start state 'q0', and final states 'fs'
 type TM a b = ([a], [b], b, [b], [TMRule a b], a, [a])
@@ -37,7 +45,47 @@ tmRules :: TM a b -> [TMRule a b]
 tmRules (_,_,_,_,rs,_,_) = rs
 
 tmStartState :: TM a b -> a
-tmStartState (_,_,_,_,_,q0,fs) = q0
+tmStartState (_,_,_,_,_,q0,_) = q0
 
 tmFinalStates :: TM a b -> [a]
 tmFinalStates (_,_,_,_,_,_,fs) = fs
+
+tmRuleValueFromKey :: (Eq a, Eq b) => TM a b -> TMRuleKey a b -> Maybe (TMRuleVal a b)
+tmRuleValueFromKey tm key
+  | length rules == 0  = Nothing
+  | otherwise          = Just (snd $ head rules)
+  where
+    rules = filter ((==key) . fst) $ tmRules tm
+
+tmMoveTape :: TMMove -> TMTape a -> TMTape a
+tmMoveTape TMLeft tape = tmTapeLeft tape
+tmMoveTape TMRight tape = tmTapeRight tape
+
+tmStepApplyRule :: TMState a b -> Maybe (TMRuleVal a b) -> Maybe (TMState a b)
+tmStepApplyRule _ Nothing = Nothing
+tmStepApplyRule (q, tape) (Just (nq, symbol, move)) = Just (nq, tmMoveTape move $ tmTapeChangeValue tape symbol)
+
+tmStep :: (Eq a, Eq b) => TM a b -> Maybe (TMState a b) -> Maybe (TMState a b)
+tmStep _ Nothing = Nothing
+tmStep tm (Just state) = tmStepApplyRule state ruleVal
+  where
+    q = fst state
+    symbol = tmTapeValue $ snd state
+    key = (q, symbol)
+    ruleVal = tmRuleValueFromKey tm key
+
+tmInitialState :: TM a b -> [b] -> TMState a b
+tmInitialState tm str = (tmStartState tm, tmTape str (tmBlankSymbol tm))
+
+tmSimulate :: (Eq a, Eq b) => TM a b -> [b] -> Maybe (TMState a b)
+tmSimulate tm str = until ended doStep $ Just $ tmInitialState tm str
+  where
+    doStep = tmStep tm
+    ended Nothing = True
+    ended (Just (q, _)) = q `elem` (tmFinalStates tm)
+
+tmCheckString :: (Eq a, Eq b) => TM a b -> [b] -> Bool
+tmCheckString tm str = let finalState = tmSimulate tm str in
+  case finalState of
+    Nothing -> False
+    Just _ -> True
--- src/Automata/TuringMachine/TMTape.hs
+++ src/Automata/TuringMachine/TMTape.hs
@@ -1,16 +1,20 @@
 module FiniteAutomata.Automata.TuringMachine.TMTape
 
 ( TMTape
-
+, tmTape
+, tmTapeRight
+, tmTapeLeft
+, tmTapeValue
+, tmTapeChangeValue
 )
 where
 
 -- |Turing Machine Tape. ((pr, v, po), b) represents a TM tape currently pointing to the value 'v', with tape prior to the value 'pr', tape post the value 'po', and blank symbol 'b'.
 type TMTape a = (([a], a, [a]), a)
 
-tmTape :: a -> [a] -> TMTape a
-tmTape b [] = (([], b, []), b)
-tmTape b (s:str) = (([], s, str), b)
+tmTape :: [a] -> a -> TMTape a
+tmTape [] b = (([], b, []), b)
+tmTape (s:str) b = (([], s, str), b)
 
 tmTapeRight :: TMTape a -> TMTape a
 tmTapeRight ((lefts, v, []), b) = ((lefts ++ [v], b, []), b)
@@ -18,4 +22,10 @@ tmTapeRight ((lefts, v, r:rights), b) = ((v:lefts, r, rights), b)
 
 tmTapeLeft :: TMTape a -> TMTape a
 tmTapeLeft (([], v, rights), b) = (([], b, v:rights), b)
-tmTapeLeft ((l:lefts, v, rights), b) = (lefts, l, v:rights)
+tmTapeLeft ((l:lefts, v, rights), b) = ((lefts, l, v:rights), b)
+
+tmTapeValue :: TMTape a -> a
+tmTapeValue ((_,v,_),_) = v
+
+tmTapeChangeValue :: TMTape a -> a -> TMTape a
+tmTapeChangeValue ((lefts, _, rights), b) v = ((lefts, v, rights), b)
